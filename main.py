95352e99147117c84a52cf7a950ce0442bd52094
JurisNomos <261876472+JurisNomos@users.noreply.github.com>
2026-02-27 20:49:24 +0800

    Add three logic function

--- main.py
+++ main.py
@@ -2,3 +2,25 @@
 import tkinter as tk
 import random
 import string
+
+# generate password functions
+def generate_password(length):
+    if length.isdigit():
+        chars = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(chars) for _ in range(int(length)))
+        return password
+    else:
+        return 'Invalid length'
+
+# validate length function
+def validate_length(text):
+    if text == '':
+        return True
+    elif text.isdigit():
+        return int(text) > 0
+    else:
+        return False
+
+# invalid length function
+def invalid_length():
+    label.config(text='Invalid length', fg='red')
